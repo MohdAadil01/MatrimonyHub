9ed74549718e8c49d7a40c2c8b88a9574cc1b15e
MohdAadil01 <mohdaadil4240@gmail.com>
2024-07-09 00:21:01 +0530

    complete vendor profile crud operation

--- api/src/routes/v1/vendor/profile.vendor.route.js
+++ api/src/routes/v1/vendor/profile.vendor.route.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const { vendorProfileController } = require("../../../controllers/vendor");
 
 router.get("/dummy", vendorProfileController.dummy);
-router.get("/view", vendorProfileController.view);
-router.put("/update", vendorProfileController.update);
-router.delete("/remove", vendorProfileController.remove);
+router.get("/view/:id", vendorProfileController.view);
+router.put("/update/:id", vendorProfileController.update);
+router.delete("/remove/:id", vendorProfileController.remove);
 
 module.exports = router;
