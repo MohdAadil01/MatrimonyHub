934a0ad81316e8835c18d884f0c5bae835bb35fa
MohdAadil01 <mohdaadil4240@gmail.com>
2024-07-08 22:30:04 +0530

    modify

--- api/src/middlewares/auth.middleware.js
+++ api/src/middlewares/auth.middleware.js
@@ -1,9 +1,9 @@
 const createHttpError = require("http-errors");
 const jwt = require("jsonwebtoken");
 
-const generateJwtToken = async (email, next) => {
+const generateJwtToken = async (userId, next) => {
   try {
-    const token = jwt.sign({ user: email }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ user: userId }, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRES_IN,
     });
     return token;
@@ -16,14 +16,14 @@ const verifyJwtToken = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization || req.headers.Authorization;
     if (!authHeader) {
-      return next(500, "Please login/signup first.");
+      return next(createHttpError(401, "Please login/signup first."));
     }
     const token = authHeader.split(" ")[1];
     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decodedData.user;
     next();
   } catch (error) {
-    return next(createHttpError(500, "Error in verifying jwt token."));
+    return next(createHttpError(403, "Error in verifying jwt token."));
   }
 };
 
