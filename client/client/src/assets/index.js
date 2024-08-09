173828a12c5a353e2965fc1189adb8d63e073757
Lokesh <rejeevseth1971@gmail.com>
2024-08-09 22:29:28 +0530

    Filteration part done for services

--- client/client/src/assets/index.js
+++ client/client/src/assets/index.js
@@ -1,1 +1,50 @@
-console.log("assets code goes here");
+export const categoryList = [
+    {
+        id: 1,
+        serviceType: 'Catering',
+        value: 'catering'
+    },
+    {
+        id: 2,
+        serviceType: 'Photography',
+        value: 'photography'
+    },
+    {
+        id: 3,
+        serviceType: 'Florist',
+        value: 'florist'
+    },
+    {
+        id: 4,
+        serviceType: 'Band',
+        value: 'band'
+    }
+]
+
+export const ratingList = [
+    {
+      id: 1,
+      value: '1',
+      label: '1🌟',
+    },
+    {
+      id: 2,
+      value: '2',
+      label: '2🌟',
+    },
+    {
+      id: 3,
+      value: '3',
+      label: '3🌟',
+    },
+    {
+      id: 4,
+      value: '4',
+      label: '4🌟',
+    },
+    {
+      id: 5,
+      value: '5',
+      label: '5🌟',
+    },
+  ];
